ae490f976985264fb153ff7d30d83bbf1c197969
Jack Doran <jcdoran33@gmail.com>
2022-09-12 21:56:14 -0400

    added javascript logic to auto-collapse the nav bar menu dropdown once a user clicks on an option

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,23 @@
 // test to see if script linked
 console.log("index.js script linked to index.html!");
 
+
+//this is the DOM ID for the hamburger menu button
+const menuButton = document.querySelector(".navbar-toggler");
+//this is the DOM ID for the div container for all the drop down options in the nav bar
+const menuOptions = document.querySelector(".navbar-collapse");
+
+//this function will auto-collapse the nav bar menu when the user clicks on one of the options
+function autoCollapse() {
+
+    //change the class attr of buttonto "navbar-toggler collapsed"
+    menuButton.setAttribute("class", "navbar-toggler collapsed");
+    //change the aria-expanded attr of button to "false"
+    menuButton.setAttribute("aria-expanded", "false");
+    //change the nav bar menu options div class to "navbar-collapse collapse"
+    menuOptions.setAttribute("class", "navbar-collapse collapse");
+
+};
+
+// click listener to trigger the auto-collapse of the nav menu
+menuOptions.addEventListener("click", autoCollapse);
